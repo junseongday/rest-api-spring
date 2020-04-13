f9214f57f715ef961e7ad0977a918e00960f628e
junseongday <junseongday@gmail.com>
2020-04-13 13:03:57 +0900

    createEvent

---
+++ src/main/java/com/junseongday/restapispring/events/EventStatus.java
@@ -0,0 +1,5 @@
+package com.junseongday.restapispring.events;
+
+public enum EventStatus {
+    DRAFT, PUBLISHED, BEGAN_ENROLLMENT, CLOSE_ENROLLMENT, STARTED, ENDED;
+}
---
+++ src/test/java/com/junseongday/restapispring/events/EventTest.java
@@ -0,0 +1,31 @@
+package com.junseongday.restapispring.events;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EventTest {
+
+    @Test
+    public void builder() {
+        Event event = Event.builder()
+                .name("Inflearn Spring REST API")
+                .description("REST API development with Spring")
+                .build();
+        assertThat(event).isNotNull();
+
+    }
+
+    @Test
+    public void javaBean() {
+        Event event = new Event();
+        String name = "Event";
+        String spring = "Spring";
+
+        event.setName(name);
+        event.setDescription(spring);
+
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(spring);
+    }
+}
