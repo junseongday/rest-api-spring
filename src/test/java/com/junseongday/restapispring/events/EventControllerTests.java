0b728ddd1214f4479f7c670e102cb75e7f6b5f41
junseongday <junseongday@gmail.com>
2020-04-13 15:21:42 +0900

    fail unkown properties

--- src/test/java/com/junseongday/restapispring/events/EventControllerTests.java
+++ src/test/java/com/junseongday/restapispring/events/EventControllerTests.java
@@ -38,6 +38,37 @@ public class EventControllerTests {
     @Test
     public void createEvent() throws Exception {
 
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2020, 4, 13, 13, 30))
+                .closeEnrollmentDateTime(LocalDateTime.of(2020, 4, 14, 13, 30))
+                .beginEventDateTime(LocalDateTime.of(2020, 4, 15, 13, 30))
+                .endEventDateTime(LocalDateTime.of(2020, 4, 16, 13, 30))
+                .basePrice(100)
+                .maxPrice(200)
+                .location("강남역 D2 스타텁 팩토리")
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("id").value(not(100)))
+                .andExpect(jsonPath("free").value(not(true)))
+                .andExpect(jsonPath("offline").value(not(true)))
+                .andExpect(jsonPath("eventStatus").value(not(EventStatus.PUBLISHED)))
+        ;
+    }
+
+    @Test
+    public void createBadRequestEvent() throws Exception {
+
         Event event = Event.builder()
                 .name("Spring")
                 .description("REST API Development with Spring")
@@ -53,22 +84,13 @@ public class EventControllerTests {
                 .offline(true)
                 .eventStatus(EventStatus.PUBLISHED)
                 .build();
-//        event.setId(10);
-//        Mockito.when(eventRepository.save(event)).thenReturn(event);
 
         mockMvc.perform(post("/api/events/")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaTypes.HAL_JSON)
                 .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists())
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-                .andExpect(jsonPath("id").value(not(100)))
-                .andExpect(jsonPath("free").value(not(true)))
-                .andExpect(jsonPath("offline").value(not(true)))
-                .andExpect(jsonPath("eventStatus").value(not(EventStatus.PUBLISHED)))
+                .andExpect(status().isBadRequest())
         ;
     }
 
