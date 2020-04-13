415573f4f6631c872d77861f8137a83a557a001f
junseongday <junseongday@gmail.com>
2020-04-13 15:11:35 +0900

    Restrict input for Post /api/events

--- src/test/java/com/junseongday/restapispring/events/EventControllerTests.java
+++ src/test/java/com/junseongday/restapispring/events/EventControllerTests.java
@@ -1,11 +1,14 @@
 package com.junseongday.restapispring.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
@@ -15,13 +18,15 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 
+import static org.hamcrest.Matchers.not;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.head;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class EventControllerTests {
 
     @Autowired
@@ -30,26 +35,26 @@ public class EventControllerTests {
     @Autowired
     ObjectMapper objectMapper;
 
-    @MockBean
-    EventRepository eventRepository;
-
     @Test
     public void createEvent() throws Exception {
 
         Event event = Event.builder()
                 .name("Spring")
                 .description("REST API Development with Spring")
-                .beginEnrollmentDateTime(LocalDateTime.of(2020,4,13,13,30))
-                .closeEnrollmentDateTime(LocalDateTime.of(2020,4,14,13,30))
-                .beginEventDateTime(LocalDateTime.of(2020,4,15,13,30))
-                .endEventDateTime(LocalDateTime.of(2020,4,16,13,30))
+                .beginEnrollmentDateTime(LocalDateTime.of(2020, 4, 13, 13, 30))
+                .closeEnrollmentDateTime(LocalDateTime.of(2020, 4, 14, 13, 30))
+                .beginEventDateTime(LocalDateTime.of(2020, 4, 15, 13, 30))
+                .endEventDateTime(LocalDateTime.of(2020, 4, 16, 13, 30))
                 .basePrice(100)
                 .maxPrice(200)
                 .location("강남역 D2 스타텁 팩토리")
-                .id(10)
-                .build()
-                ;
-        Mockito.when(eventRepository.save(event)).thenReturn(event);
+                .id(100)
+                .free(true)
+                .offline(true)
+                .eventStatus(EventStatus.PUBLISHED)
+                .build();
+//        event.setId(10);
+//        Mockito.when(eventRepository.save(event)).thenReturn(event);
 
         mockMvc.perform(post("/api/events/")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -57,9 +62,13 @@ public class EventControllerTests {
                 .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
                 .andExpect(status().isCreated())
-        .andExpect(jsonPath("id").exists())
-        .andExpect(header().exists(HttpHeaders.LOCATION))
-        .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("id").value(not(100)))
+                .andExpect(jsonPath("free").value(not(true)))
+                .andExpect(jsonPath("offline").value(not(true)))
+                .andExpect(jsonPath("eventStatus").value(not(EventStatus.PUBLISHED)))
         ;
     }
 
