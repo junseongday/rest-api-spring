436249f661544e28f9edec14e66edbd5f6b05cad
junseongday <junseongday@gmail.com>
2020-04-16 11:02:37 +0900

    apply springsecurity

---
+++ src/test/java/com/junseongday/restapispring/accounts/AccountServiceTest.java
@@ -0,0 +1,47 @@
+package com.junseongday.restapispring.accounts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class AccountServiceTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    public void findByUserName() {
+        // Given
+        String username = "junseong@gmail.com";
+        String pass = "junseon";
+        Account account = Account.builder()
+                .email(username)
+                .password(pass)
+                .roles(Set.of(AccoutnRole.ADMIN,AccoutnRole.USER))
+                .build();
+        accountRepository.save(account);
+
+        // When
+        UserDetailsService userDetailsService = (UserDetailsService) accountService;
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        // Then
+        assertThat(userDetails.getPassword()).isEqualTo(pass);
+    }
+}
